cb9b7f3435d7ea0a0dcce9bf9eb52aabd6fa2806
pedro <contatopedrogurgel@gmail.com>
2025-03-22 16:32:25 -0300

    DUBI-6: creating front-end for question

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,26 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <form method="post" action="{{route('question.store')}}">
+                @csrf
+                <div class="py-3">
+                    <label for="question" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your question</label>
+                    <textarea name="question" id="question" rows="4" class="block  p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here...">{{old('question')}}</textarea>
+
+                </div>
+                <button type="submit" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Save</button>
+                <button type="reset" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Reset</button>
+
+            </form>
+            @error('question')
+            <div class="text-red-500 mt-2 text-sm">
+               <span>{{ $message }}</span>
+            </div>
+            @enderror
+        </div>
+
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
