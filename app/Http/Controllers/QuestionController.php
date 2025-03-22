835b404f24159486290ddb2482ef8ede7087fd57
pedro <contatopedrogurgel@gmail.com>
2025-03-21 23:02:09 -0300

    DUBI-6: adding endWithQuestionMark rule

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
-use Closure;
+use App\Rules\EndWithQuestionMarkRule;
 use Illuminate\Http\{RedirectResponse};
 
 class QuestionController extends Controller
@@ -14,11 +14,7 @@ class QuestionController extends Controller
         $atributes = request()->validate([
             'question' => ['required',
                 'min:10',
-                function (string $attribute, mixed $value, Closure $fail) {
-                    if ($value[strlen($value) - 1] !== '?') {
-                        $fail('Are you sure that the question ends with a question mark?');
-                    }
-                },
+                new EndWithQuestionMarkRule(),
             ],
 
         ]);
