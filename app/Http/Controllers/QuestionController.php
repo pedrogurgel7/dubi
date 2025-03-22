41b01ab63912c7667475619af95a3b7ee3f65938
pedro <contatopedrogurgel@gmail.com>
2025-03-21 22:58:06 -0300

    DUBI-6: should check if ends with question mark ?

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use Closure;
 use Illuminate\Http\{RedirectResponse};
 
 class QuestionController extends Controller
@@ -10,11 +11,19 @@ class QuestionController extends Controller
     public function store(): RedirectResponse
     {
 
-        Question::query()->create(
-            request()->validate([
-                'question' => 'required|min:10|max:265',
+        $atributes = request()->validate([
+            'question' => ['required',
+                'min:10',
+                function (string $attribute, mixed $value, Closure $fail) {
+                    if ($value[strlen($value) - 1] !== '?') {
+                        $fail('Are you sure that the question ends with a question mark?');
+                    }
+                },
+            ],
 
-            ])
+        ]);
+        Question::query()->create(
+            $atributes
         );
 
         return to_route('dashboard');
--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -11,18 +11,26 @@ test('it should be able to create a new question bigger than 255 caracters', fun
 
     //Act
     $request = post(route('question.store'), [
-        'question' => str_repeat('*', 260) . '?',
+        'question' => str_repeat('*', 255) . '?',
     ]);
     //Assert
 
     $request->assertRedirect(route('dashboard'));
     assertDatabaseCount('questions', 1);
 
-    assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?']);
+    assertDatabaseHas('questions', ['question' => str_repeat('*', 255) . '?']);
 
 });
 
 it('should check if ends with question mark ?', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 10),
+    ]);
+
+    $request->assertSessionHasErrors(['question' => 'Are you sure that the question ends with a question mark?']);
 
 });
 
