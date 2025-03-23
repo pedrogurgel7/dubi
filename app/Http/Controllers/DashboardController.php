2191aa23eb22f1d75cfa7ed131fa15a719900411
pedro <contatopedrogurgel@gmail.com>
2025-03-23 13:05:11 -0300

    DUBI-9: returns a list of questions

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,6 +12,8 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request): \Illuminate\View\View
     {
+
+        //dd(Question::all());
         return view('dashboard', [
             'questions' => Question::all(),
         ]);
--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -5,5 +5,5 @@
 )
 
 <div class="rounded bg-white shadow shadow-blue-500/50  p-4 mb-4">
-    {{$item}}
+    {!! $item !!}
 </div>
